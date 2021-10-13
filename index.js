ec8e9c2e64b85908a5a0547c2ce0601a519b3bbe
GedasBu <86909912+GedasBu@users.noreply.github.com>
2021-10-13 12:24:09 +0300

    upd

--- index.js
+++ index.js
@@ -1,7 +1,39 @@
 const data = require('./lib/data.js');
 
-console.log(data);
-data.create();
-data.read();
-data.update();
-data.delete();
+// console.log(data);
+data.create('users','petras',{name: 'Petras', age: 99},(state,msg)=>{
+    console.log('Kuriamas irasas')
+if (state){
+    console.log(msg);
+} else {
+    console.error(msg);
+}
+console.log('Sekantys zingzniai po bandymo sukurti faila')
+} );
+
+// data.read('users', 'petras');
+// data.update();
+// data.delete();
+
+// data.create('users', 'labas', {name: "Marsietis"})
+
+data.create('users','jonas',{name: 'Jonas', age: 69},(state,msg)=>{
+   
+    if (state){
+        console.log(msg);
+    } else {
+        console.error(msg);
+    }
+    console.log('Sekantys zingzniai po bandymo sukurti faila')
+    } );
+
+
+data.create('books','pasaka',{name: 'Pasakos', puslapiai: 69},(state,msg)=>{
+   
+        if (state){
+            console.log(msg);
+        } else {
+            console.error(msg);
+        }
+        console.log('Sekantys zingzniai po bandymo sukurti faila')
+        } );
